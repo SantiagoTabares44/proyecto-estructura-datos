8b74b8c3d2972fc1e24de469402b742dfbcc7b26
Santiago Tabares <santiago.tabares44@correo.tdea.edu.co>
2024-10-11 20:23:42 -0500

    Add - Organizacion de datos - creacion de la clase nodo vacia

---
+++ ProyectoEstructura/src/main/java/co/edu/tdea/edd/EddApplication.java
@@ -0,0 +1,19 @@
+package co.edu.tdea.edd;
+
+
+public class EddApplication {
+
+    public static void main( String[] args ) {
+       load();
+        menu();
+    }
+
+    public static void load(){
+        System.out.println("Cargando");
+    }
+
+    public static void menu(){
+        System.out.println("Atender Paciente:");
+
+    }
+}
